2c0e60faa259dc0483d649723e5935700f6e6cb6
Marco Del Valle <marco@dyadstudios.com>
2017-11-03 14:22:49 -0700

    Lesson 24: Bidirectional relations.

--- schema/schema.js
+++ schema/schema.js
@@ -6,17 +6,41 @@ const {
 	GraphQLObjectType,
 	GraphQLString,
 	GraphQLInt,
-	GraphQLSchema
+	GraphQLSchema,
+	GraphQLList
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	fields: () => ({
+		id: {type: GraphQLString},
+		name: {type: GraphQLString},
+		description: {type: GraphQLString},
+		users: {
+			type: new GraphQLList(UserType),	// This tells GQL to expect an array of <UserType>s
+			resolve(parentValue){
+				return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+					.then(res => res.data);
+			}
+		}
+	})
+});
 
 // This object instructs GraphQL about what a user object looks like
 const UserType = new GraphQLObjectType({
 	name: 'User',
-	fields: {
+	fields: () => ({
 		id: {type: GraphQLString},
 		firstName: {type: GraphQLString},
-		age: {type: GraphQLInt}
-	},
+		age: {type: GraphQLInt},
+		company: {
+			type: CompanyType,
+			resolve(parentValue, args){
+				return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+					.then(res => res.data);
+			}
+		}
+	}),
 });
 
 // Root Query is an entry point into the data
@@ -25,14 +49,20 @@ const RootQuery = new GraphQLObjectType({
 	fields: {
 		user: {
 			type: UserType,
-			args: {
-				id: {type: GraphQLString}
-			},
+			args: {id: {type: GraphQLString}},
 
-			// Resolve fn is where we go into DB and find the actual data we're looking for
+			// Resolve fn is where we go into DB and find the hard data we're looking for
 			resolve(parentValue, args){
 				return axios.get(`http://localhost:3000/users/${args.id}`)
 					// Axios nests results in .data so we must dig it out
+					.then(resp => resp.data);
+			}
+		},
+		company: {
+			type: CompanyType,
+			args: {id: {type: GraphQLString}},
+			resolve(parentValue, args){
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
 					.then(resp => resp.data);
 			}
 		}
@@ -42,3 +72,27 @@ const RootQuery = new GraphQLObjectType({
 module.exports = new GraphQLSchema({
 	query: RootQuery
 });
+
+/************* SAMPLE QUERY FOR GraphiQL *************
+
+query findCompany{
+  company1: company(id: "1"){
+    ...companyDetails
+  },
+  
+  company2: company(id: "2"){
+    ...companyDetails
+  }
+}
+
+fragment companyDetails on Company{
+  id,
+  name,
+  description,  
+  users{
+    firstName,
+    id,
+    age,
+  }
+}
+*/
